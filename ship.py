f3faf70934451f3316f33a9e7b5e94895f91f32d
AlbertoL1990 <a1990alpalo@gmail.com>
2026-07-19 20:17:12 -0400

    added fire ammo, limit to fire and sound effects

--- ship.py
+++ ship.py
@@ -3,10 +3,13 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+    from arsenal import Arsenal 
+
+
 
 class Ship: 
     
-    def __init__(self, game: 'AlienInvasion'):
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal') -> None:
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -23,10 +26,14 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
         self.x = float(self.rect.x)
+        self.arsenal = arsenal
 
     def update(self):
         # Updating the position of the ship
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
         
+    def _update_ship_movement(self):    
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
@@ -37,7 +44,10 @@ class Ship:
 
         
 
-
-
     def draw(self):
+        self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+        return self.arsenal.fire_bullet()
+    
