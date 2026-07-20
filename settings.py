4ec4891b2c08d5f0bf13d9dcdba8270d9593d3fa
AlbertoL1990 <a1990alpalo@gmail.com>
2026-07-19 21:47:50 -0400

    add the drop of the alien fleet

--- settings.py
+++ settings.py
@@ -45,10 +45,11 @@ class Settings:
             /"images"
             /"enemy_4.png"
         )
-        self.fleet_speed = 2
+        self.fleet_speed = 5
         self.alien_w = 40
         self.alien_h = 40
         self.fleet_direction = 1
+        self.fleet_drop_speed = 40
         
         self.laser_sound = (
             Path.cwd()
